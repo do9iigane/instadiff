3c93f2718fe73c9a7ffd2373c1d57058822d45b4
毒眼鏡 <>
2012-06-04 21:58:46 +0900

    ここまでコミット

--- index.php
+++ index.php
@@ -16,17 +16,7 @@
  * latest updates
  */
 require_once 'Instagram.php';
-
-/**
- * Configuration params, make sure to write exactly the ones
- * instagram provide you at http://instagr.am/developer/
- */
-$config = array(
-        'client_id' => 'f080fc1fb4b447a49795275a7fcb9036',
-        'client_secret' => '6e7b34c2d54f4dc984254825267731d8',
-        'grant_type' => 'authorization_code',
-        'redirect_uri' => 'http://instadiff.herokuapp.com/callback.php',
-     );
+require_once 'config.inc.php';
 
 /**
  * This is how a wrong response looks like
