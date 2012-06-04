756dde3aaf8f3dd237b41259d3c429ef2d992f1b
毒眼鏡 <>
2012-06-04 21:47:20 +0900

    調整

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ $config = array(
         'client_id' => 'f080fc1fb4b447a49795275a7fcb9036',
         'client_secret' => '6e7b34c2d54f4dc984254825267731d8',
         'grant_type' => 'authorization_code',
-        'redirect_uri' => 'http://instadiff.herokuapp.com/',
+        'redirect_uri' => 'http://instadiff.herokuapp.com/callback.php',
      );
 
 /**
