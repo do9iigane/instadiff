ba0131f14d00d0f71a2b9d6c8078c9974273bfca
毒眼鏡 <>
2012-06-26 14:50:33 +0900

    ff,chrome,IE対応

--- ac.php
+++ ac.php
@@ -32,11 +32,56 @@
 		</STYLE>
 		<script>
 		    $(document).ready(function(){
-		        $('div #panel1').click(function(){
-		            window.console.log();
-		        })
-		        
+		        $('div.panel').each(function(){
+		            panel = $(this);
+		            panel.click(function(){
+		                $(this).css('background-color', changecolor($(this).css('background-color')));
+		            })
+                });
 		    })
+		    
+            if($('div.panel1').css('background-color')==$('div.panel2').css('background-color')==$('div.panel1').css('background-color')){
+                window.console.log('BINGO!');
+            }		    
+		    
+		    //colorchange method
+		    function changecolor(color){
+		        window.console.log(color);
+		        switch (true) {
+		            case color=='transparent':
+		            case color=='rgba(0, 0, 0, 0)'://chrome対策
+		            color='#ff0000';
+		            break;
+                    case color=='rgb(255, 0, 0)':
+                    case color=='#ff0000'://IE対策
+                    color='#0000ff';
+                    break;
+                    case color=='rgb(0, 0, 255)':
+                    case color=='#0000ff'://IE対策
+                    color='#008000';
+                    break;
+                    case color=='rgb(0, 128, 0)':
+                    case color=='#008000'://IE対策
+                    color='#ffff00';
+                    break;
+                    case color=='rgb(255, 255, 0)':
+                    case color=='#ffff00'://IE対策
+                    color='#800080';
+                    break;
+                    case color=='rgb(128, 0, 128)':
+                    case color=='#800080'://IE対策
+                    color='#ff00ff';
+                    break;
+                    case color=='rgb(255, 0, 255)':
+                    case color=='#ff00ff'://IE対策
+                    color='transparent';
+                    break;
+		            default:
+		            color='transparent';
+		            break;
+		        }
+		        return color;
+		    }
 		</script>
 	</head>
 	<body>
