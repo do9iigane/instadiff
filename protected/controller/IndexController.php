857e1706f90d682e79aa4006cc275c66e9118b76
do9iigane <dokumegane@interestic.com>
2012-11-28 17:11:48 +0900

    session調整

--- protected/controller/IndexController.php
+++ protected/controller/IndexController.php
@@ -2,22 +2,25 @@
 
 class IndexController extends DooController {
 
+    function __construct() {
+        //instaconfigを読み込み
+        $this->conf = Doo::conf();
+        $this->session = Doo::session($this->conf->SERVER_NAME);
+    }
+
     function index()
     {
         //instaclassを読み込み
         require_once THIRDPATH . 'instagramClass.php';
-        //instaconfigを読み込み
-        $dooconf = Doo::conf();
         $this->session = Doo::session($this->conf->SERVER_NAME);
+        require_once $this->conf->SITE_PATH . $this->conf->PROTECTED_FOLDER . "config/instagram.conf.php";
         
-        require_once $dooconf->SITE_PATH . $dooconf->PROTECTED_FOLDER . "config/instagram.conf.php";
-
         //認証が通っていれば、callbackに渡す
         if (isset($this->session->InstagramAccessToken) && !empty($this->session->InstagramAccessToken)) {
             header('Location: callback');
             die();
         }
-
+        
         $instagram = new Instagram($config);
 
         //instaに認証を渡す
